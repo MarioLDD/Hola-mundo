24d2285c8c9c921cace4373daca87f2adbbcb3f8
MarioLDD <112513242+MarioLDD@users.noreply.github.com>
2022-09-03 03:20:08 -0300

    hola mundo
    primer hola mundo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+public class prueba
+{
+    static void Main(string[] args)
+    {
+        Console.ReadLine();
+        Console.WriteLine("pop");
+    }
+}
+   
+   
