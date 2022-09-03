a37a33a41710a945347d3ba56928e3733d751c73
MarioLDD <112513242+MarioLDD@users.noreply.github.com>
2022-09-03 04:25:08 -0300

    segunda iteracion
    cambio codigo completo

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
-
-
-public class prueba
+﻿using System; // Define the System package  
+namespace ConsoleApp3 // Project name or Folder  
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.ReadLine();
-        Console.WriteLine("pop");
+        static void Main(string[] args) // Defining the main function  
+        {
+            string name; // string variable name  
+            Console.WriteLine("Hello, what is your name?");
+            name = Console.ReadLine(); // takes an input from the user  
+            Console.WriteLine("Hi! " + name + " Welcome to the JavaTpoint"); // print the output  
+        }
     }
 }
-   
-   
